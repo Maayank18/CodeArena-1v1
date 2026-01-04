d441cb47a1621704f62547c38007eb7baa2bd88e
Maayank18 <maayank51518@gmail.com>
2026-01-04 20:52:08 +0530

    remove un-necessary noisy code and fixed important patches

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -1,26 +1,3 @@
-// frontend/src/components/Logo.jsx
-// import React from 'react';
-// // Make sure this path is correct for your setup
-// import logoShield from '../assets/CodeArenaLogo.png'; 
-
-// export const Logo = ({ className }) => (
-//   <div className={`flex items-center gap-3 font-bold text-2xl tracking-tighter ${className}`}>
-//     <img 
-//       src={logoShield} 
-//       alt="CodeArena Logo" 
-//       // ---- THE FIX IS HERE ----
-//       // Changed from h-12 w-12 to h-16 w-16
-//       className="h-20 w-20 object-contain drop-shadow-[0_0_15px_rgba(74,238,136,0.4)]" 
-//       // -------------------------
-//     />
-//     <span className="transition-colors duration-300">
-//       Code<span className="text-accent">Arena</span>1v1
-//     </span>
-//   </div>
-// );
-
-
-
 // RESPONSIVE TWEAK CHANGES 
 import React from 'react';
 // Make sure this path is correct for your setup
