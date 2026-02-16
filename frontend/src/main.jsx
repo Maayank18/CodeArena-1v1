15ca0a40f2077c0cc65ce46e6e8cbb7ade085748
Maayank18 <maayank51518@gmail.com>
2026-02-16 21:14:07 +0530

    updated the app.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,14 @@ import { Toaster } from 'react-hot-toast';
 import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext'; // Import
 import './index.css';
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <ThemeProvider> {/* WRAP HERE */}
         <Toaster position="top-right" />
         <App />
+        <Analytics />
       </ThemeProvider>
     </BrowserRouter>
 );
